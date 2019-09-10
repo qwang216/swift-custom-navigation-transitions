edc8969f4a6f7e61183adbf38636079a16ae5b4e
Cary Zhou <cary.zhou@rallyhealth.com>
2019-09-10 10:39:26 -0400

    Initial Commit

---
+++ TransitionAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionAnimation
+//
+//  Created by Cary Zhou on 9/10/19.
+//  Copyright © 2019 Cary Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
